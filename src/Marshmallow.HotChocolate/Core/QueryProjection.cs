1add40d651a4c444b11c75e93617fc4e4563b95f
Rodrigo Rodrigues <rs.rodrigues@mrv.com.br>
2020-01-18 16:03:38 -0300

    Remoção do memory cache

--- src/Marshmallow.HotChocolate/Core/QueryProjection.cs
+++ src/Marshmallow.HotChocolate/Core/QueryProjection.cs
@@ -11,9 +11,8 @@ namespace Marshmallow.HotChocolate.Core
     class QueryProjection : IQueryProjection
     {
         private readonly IReadOnlyQueryRequest _readOnlyQueryRequest;
-        private readonly IMemoryCache _memoryCache;
 
-        public QueryProjection(IHttpContextAccessor httpContextAccessor, IMemoryCache memoryCache = null)
+        public QueryProjection(IHttpContextAccessor httpContextAccessor)
         {
             if (!httpContextAccessor.HttpContext.Items.ContainsKey("graph"))
             {
@@ -21,28 +20,11 @@ namespace Marshmallow.HotChocolate.Core
             }
 
             _readOnlyQueryRequest = httpContextAccessor.HttpContext.Items["graph"] as IReadOnlyQueryRequest;
-            _memoryCache = memoryCache;
         }
 
         public Expression<Func<TEntity, dynamic>> CreateExpression<TEntity>()
         {
-            if (_memoryCache != null)
-            {
-                if (_memoryCache.TryGetValue(_readOnlyQueryRequest.QueryHash, out Expression<Func<TEntity, dynamic>> expression))
-                {
-                    return expression;
-                }
-
-                Expression<Func<TEntity, dynamic>> generatedExpression = CreateExpression<TEntity>(_readOnlyQueryRequest.Query);
-
-                _memoryCache.Set(_readOnlyQueryRequest.QueryHash, generatedExpression);
-
-                return generatedExpression;
-            }
-            else
-            {
-                return CreateExpression<TEntity>(_readOnlyQueryRequest.Query);
-            }
+            return CreateExpression<TEntity>(_readOnlyQueryRequest.Query);
         }
 
         protected virtual Expression<Func<TEntity, dynamic>> CreateExpression<TEntity>(IQuery query)
