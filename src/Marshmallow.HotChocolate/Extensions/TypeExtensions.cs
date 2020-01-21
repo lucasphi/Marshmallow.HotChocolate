0fcde468263c5b9b3ffee053d48e797ce7172118
Lucas Phillip <lucas.leite@dtidigital.com.br>
2020-01-21 14:39:01 -0300

    Adding type extensions

---
+++ src/Marshmallow.HotChocolate/Extensions/TypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Marshmallow.HotChocolate
+{
+    static class TypeExtensions
+    {
+        public static bool IsComplex(this Type type)
+        {
+            return !type.IsPrimitive
+                && !type.IsGenericType
+                && !type.IsEnum
+                && type != typeof(string)
+                && type != typeof(DateTime)
+                && type != typeof(Guid);
+        }
+
+        public static bool IsGenericCollection(this Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var enumerableType = typeof(IEnumerable<>);
+                return type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == enumerableType);
+            }
+
+            return false;
+        }
+    }
+}
