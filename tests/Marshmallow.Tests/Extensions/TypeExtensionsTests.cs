9bd64fe1fd585d4ccc758867d9544fe89e1693ff
Lucas Phillip <lucasphi@gmail.com>
2020-01-21 14:44:40 -0300

    Refactoring type extensions (#8)
    * Adding type extensions

---
+++ tests/Marshmallow.Tests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Marshmallow.HotChocolate;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+
+[assembly: InternalsVisibleTo("Marshmallow.Tests")]
+namespace Marshmallow.Tests.Extensions
+{
+    public class TypeExtensionsTests
+    {
+        [Fact]
+        public void ListGenericCollectionTest()
+        {
+            var test = new List<string>();
+
+            var result = test.GetType().IsGenericCollection();
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CollectionGenericCollectionTest()
+        {
+            var test = new Collection<string>();
+
+            var result = test.GetType().IsGenericCollection();
+
+            result.Should().BeTrue();
+        }
+    }
+}
